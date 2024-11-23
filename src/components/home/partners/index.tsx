b547fa3b3f5def369d740bdca16cd6750cd7b070
AsimMahmudov <AsimMahmudov>
2024-11-23 13:04:27 +0600

    partners

--- src/components/home/partners/index.tsx
+++ src/components/home/partners/index.tsx
@@ -18,38 +18,23 @@ const data = [
 	{ image: partners6 },
 	 
 ];
- 
-// const moveLeft = `
-//   @keyframes moveLeft {
-//     0% {
-//       transform: translateX(100%);
-//     }
-//     100% {
-//       transform: translateX(-100%);
-//     }
-//   }
-// `;
+  
 
 const Partners = () => {
 	return (
 		<Box display={{ base: "none", md: "flex" }} py={20}>
-			<Box className="container" style={{ overflow: "hidden" }}>
+			<Box className="container" >
 				<Text textAlign="center" py={4} fontSize={52} fontWeight={600}>
 					Партнеры
 				</Text>
 			 
-				{/* <style jsx global>{moveLeft}</style> */}
 				<Flex
 					gap={2}
-					style={{
-						animation: "moveLeft 30s linear infinite",
-						display: "flex",
-						width: "max-content",  
-					}}
+				 
 				>
 					{data.map((el, index) => (
-						<Box key={index} flexShrink={0}> 
-							<Image style={{width:"100%", height:"71px"}} src={el.image} alt="img" />
+						<Box key={index}  > 
+							<Image style={{width:"100%", height:"100%"}} src={el.image} alt="img" />
 						</Box>
 					))}
 				</Flex>
