e2b23ae409ffd3acca5858ca34552d7fd30ff073
AsimMahmudov <AsimMahmudov>
2024-11-27 12:43:33 +0600

    about

--- src/components/home/about/index.tsx
+++ src/components/home/about/index.tsx
@@ -4,6 +4,8 @@ import { FiCheck } from "react-icons/fi";
 import ava from "@/assets/img/amangeldiev.png";
 import Image from "next/image";
 import Link from "next/link";
+import logo from "@/assets/img/aboutlogo.png";
+import text from "@/assets/img/abouttext.png";
 
 const About = () => {
 	const data = [
@@ -24,53 +26,75 @@ const About = () => {
 					flexDirection={{ base: "column", md: "row" }}
 					gap={10}
 					justifyContent="space-between">
-					<Box
+					<Flex
+						justifyContent={{ base: "center", md: "space-between" }}
+						alignItems="center"
 						w="100%"
 						data-aos="fade-up-right"
 						p={{ base: "40px 15px", md: "40px 50px" }}
 						borderRadius="17px"
 						bg="#191919"
+						flexDirection={{ base: "column", md: "row" }}
 						color="white">
-						<Text fontSize={{ base: 30, md: 52 }} fontWeight={600}>
-							О компании
-						</Text>
-						<Text
-							mt={4}
-							w={{ base: "100%", md: 540 }}
-							fontSize={18}
-							fontWeight={400}>
-							Наша компания с 2014 года занимается производством и установкой
-							комплексных инженерных сооружении в Кыргызстане, а также в странах
-							СНГ и Персидского Залива.
-						</Text>
-						<Flex mt={6} flexDirection="column" gap={6}>
-							{data.map((el, index) => (
-								<Flex gap={3} alignItems="center" key={index}>
-									<Text
-										bg="#77777727"
-										p="1px"
-										borderRadius={4}
-										border="solid 2px #797979">
-										<FiCheck />
-									</Text>
-									<Text color="#aaaaaa" fontSize={16} fontWeight={400}>
-										{el.text}
-									</Text>
-								</Flex>
-							))}
+						<Box>
+							<Text fontSize={{ base: 30, md: 52 }} fontWeight={600}>
+								О компании
+							</Text>
+							<Text
+								mt={4}
+								w={{ base: "100%", md: 540 }}
+								fontSize={18}
+								fontWeight={400}>
+								Наша компания с 2014 года занимается производством и установкой
+								комплексных инженерных сооружении в Кыргызстане, а также в
+								странах СНГ и Персидского Залива.
+							</Text>
+							<Flex mt={6} flexDirection="column" gap={6}>
+								{data.map((el, index) => (
+									<Flex gap={3} alignItems="center" key={index}>
+										<Text
+											bg="#77777727"
+											p="1px"
+											borderRadius={4}
+											border="solid 2px #797979">
+											<FiCheck />
+										</Text>
+										<Text color="#aaaaaa" fontSize={16} fontWeight={400}>
+											{el.text}
+										</Text>
+									</Flex>
+								))}
+							</Flex>
+							<Link href={"#contact"}>
+								<Button
+									mt={8}
+									w={330}
+									h="60px"
+									borderRadius={50}
+									bg="#0E6B56"
+									color="white">
+									Оставить заявку
+								</Button>
+							</Link>
+						</Box>
+						<Flex paddingRight={{ base: 0, md: 20}} justifyContent="center" mt={{ base: 10, md: 0}} flexDir="column"  >
+							<Box w={{ base: "100%", md: 240 }} h="100%">
+								<Image
+									style={{ width: "240px", height: "240px" }}
+									src={logo}
+									alt="img"
+								/>
+							</Box>
+
+							<Box w={{ base: "100%", md: 240 }} h="100%">
+								<Image
+									style={{ width: "240px", height: "60px" }}
+									src={text}
+									alt="img"
+								/>
+							</Box>
 						</Flex>
-						<Link href={"#contact"}>
-							<Button
-								mt={8}
-								w={330}
-								h="60px"
-								borderRadius={50}
-								bg="#0E6B56"
-								color="white">
-								Оставить заявку
-							</Button>
-						</Link>
-					</Box>
+					</Flex>
 					{/* <Box
 						data-aos="fade-up-left"
 						textAlign="center"
---
+++ src/global.d.ts
@@ -0,0 +1,8 @@
+declare global {
+  interface Window {
+    DG: any; // Replace `any` with the proper type if available.
+  }
+}
+
+// Required to make this file a module
+export {};
